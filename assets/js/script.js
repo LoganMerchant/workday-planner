ce37338bf80d6e7fadc6588b8e34513ab709aad6
Logan Merchant <merchantclogan@gmail.com>
2020-08-27 16:35:47 -0600

    add color coding based on time

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,16 +1,41 @@
 var currentDay = function() {
-    var date = moment().local().format('dddd, MMMM do, YYYY');
+    var date = moment().format('dddd, MMMM Do, YYYY');
     $("#currentDay").text(date);
 };
 
 var timeBlockSet = function() {
-    var timeBlock = $('.hour'); 
+    var timeBlock = $('.hour p'); 
     var hour = 9;
 
     timeBlock.each(function() {
-        $(this).text(moment().set('hour', hour).format('ha'));
-        timeBlock.addClass('text-right');
+        var workHour = moment().set('hour', hour).format('ha');
+
+        $(this).text(workHour);
         hour++;
+    });
+
+    timeCheck();
+};
+
+var timeCheck = function () {
+    var description = $('.time-block .description');
+    var hour = 9;
+
+    description.each(function() {
+        var time = moment().set('hour', hour);
+
+        if (moment().isAfter(time)) {
+            $(this).addClass('past');
+
+        } else if (moment().isSame(time)) {
+            $(this).addClass('present');
+
+        } else if (moment().isBefore(time)) {
+            $(this).addClass('future');
+        };   
+        hour++;
+
+        console.log($(this));
     });
 };
 
